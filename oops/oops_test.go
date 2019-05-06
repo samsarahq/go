63e93ac62ca869731a402f8c4c70b7291babff14
Stephen Wan <stephen@stephenwan.com>
2019-05-06 11:24:32 -0700

    oops: add tests for Is and As behavior
    Also update test for printing and Frames behavior - the trace now includes information
    all the way down to the base.

--- oops/oops_test.go
+++ oops/oops_test.go
@@ -95,13 +95,20 @@ func (e *baseErr) Error() string {
 	return "base"
 }
 
-func chain() error {
+func oopsChain() error {
 	base := &baseErr{}
 	a := oops.Wrapf(base, "a")
 	b := oops.Wrapf(a, "b")
 	middle := &wrapperErr{inner: b}
 	c := oops.Wrapf(middle, "c")
 	return oops.Wrapf(c, "d")
+}
+
+func chain() error {
+	base := &baseErr{}
+	a := oops.Wrapf(base, "a")
+	b := oops.Wrapf(a, "b")
+	return &wrapperErr{inner: b}
 }
 
 func TestRecoverNil(t *testing.T) {
@@ -399,18 +406,32 @@ testing.tRunner
 		},
 		{
 			Title: "chain oops",
-			Error: chain(),
+			Error: oopsChain(),
 			Short: "wrapper",
-			Verbose: `wrapper
+			Verbose: `base
 
-github.com/samsarahq/go/oops_test.chain: c
+github.com/samsarahq/go/oops_test.oopsChain: a
 	github.com/samsarahq/go/oops/oops_test.go:123
 github.com/samsarahq/go/oops_test.TestErrors
 	github.com/samsarahq/go/oops/oops_test.go:123
 testing.tRunner
 	testing/testing.go:123
 
-github.com/samsarahq/go/oops_test.chain: d
+github.com/samsarahq/go/oops_test.oopsChain: b
+	github.com/samsarahq/go/oops/oops_test.go:123
+github.com/samsarahq/go/oops_test.TestErrors
+	github.com/samsarahq/go/oops/oops_test.go:123
+testing.tRunner
+	testing/testing.go:123
+
+github.com/samsarahq/go/oops_test.oopsChain: c
+	github.com/samsarahq/go/oops/oops_test.go:123
+github.com/samsarahq/go/oops_test.TestErrors
+	github.com/samsarahq/go/oops/oops_test.go:123
+testing.tRunner
+	testing/testing.go:123
+
+github.com/samsarahq/go/oops_test.oopsChain: d
 	github.com/samsarahq/go/oops/oops_test.go:123
 github.com/samsarahq/go/oops_test.TestErrors
 	github.com/samsarahq/go/oops/oops_test.go:123
@@ -439,6 +460,31 @@ testing.tRunner
 	}
 }
 
+func TestFrames(t *testing.T) {
+	testCases := []struct {
+		description string
+		err         error
+		expected    [][]oops.Frame
+	}{
+		{
+			description: "non-oops chain",
+			err:         chain(),
+			expected:    [][]oops.Frame{[]oops.Frame{oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.chain", Line: 109, Reason: "a"}, oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.TestFrames", Line: 471, Reason: ""}, oops.Frame{File: "testing/testing.go", Function: "testing.tRunner", Line: 827, Reason: ""}}, []oops.Frame{oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.chain", Line: 110, Reason: "b"}, oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.TestFrames", Line: 471, Reason: ""}, oops.Frame{File: "testing/testing.go", Function: "testing.tRunner", Line: 827, Reason: ""}}},
+		},
+		{
+			description: "oops chain",
+			err:         oopsChain(),
+			expected:    [][]oops.Frame{[]oops.Frame{oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.oopsChain", Line: 100, Reason: "a"}, oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.TestFrames", Line: 476, Reason: ""}, oops.Frame{File: "testing/testing.go", Function: "testing.tRunner", Line: 827, Reason: ""}}, []oops.Frame{oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.oopsChain", Line: 101, Reason: "b"}, oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.TestFrames", Line: 476, Reason: ""}, oops.Frame{File: "testing/testing.go", Function: "testing.tRunner", Line: 827, Reason: ""}}, []oops.Frame{oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.oopsChain", Line: 103, Reason: "c"}, oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.TestFrames", Line: 476, Reason: ""}, oops.Frame{File: "testing/testing.go", Function: "testing.tRunner", Line: 827, Reason: ""}}, []oops.Frame{oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.oopsChain", Line: 104, Reason: "d"}, oops.Frame{File: "github.com/samsarahq/go/oops/oops_test.go", Function: "github.com/samsarahq/go/oops_test.TestFrames", Line: 476, Reason: ""}, oops.Frame{File: "testing/testing.go", Function: "testing.tRunner", Line: 827, Reason: ""}}},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			assert.Equal(t, tc.expected, oops.Frames(tc.err))
+		})
+	}
+}
+
 func TestCause(t *testing.T) {
 	base := &baseErr{}
 	a := oops.Wrapf(base, "a")
@@ -454,4 +500,44 @@ func TestCause(t *testing.T) {
 	assert.Equal(t, middle, oops.Cause(middle))
 	assert.Equal(t, middle, oops.Cause(c))
 	assert.Equal(t, middle, oops.Cause(d))
+}
+
+func TestIs(t *testing.T) {
+	base := errors.New("base")
+	a := oops.Wrapf(base, "a")
+	b := oops.Wrapf(a, "b")
+	middle := &wrapperErr{inner: b}
+	c := oops.Wrapf(middle, "c")
+	d := oops.Wrapf(c, "d")
+
+	assert.True(t, oops.Is(a, base))
+	assert.True(t, oops.Is(b, base))
+	assert.True(t, oops.Is(middle, base))
+	assert.True(t, oops.Is(c, base))
+	assert.True(t, oops.Is(d, base))
+}
+
+func TestAs(t *testing.T) {
+	base := &baseErr{}
+	a := oops.Wrapf(base, "a")
+	b := oops.Wrapf(a, "b")
+	middle := &wrapperErr{inner: b}
+	c := oops.Wrapf(middle, "c")
+	d := oops.Wrapf(c, "d")
+
+	var checkBase *baseErr
+	assert.True(t, oops.As(a, &checkBase))
+	assert.Equal(t, base, checkBase)
+
+	checkBase = nil
+	assert.True(t, oops.As(b, &checkBase))
+	assert.Equal(t, base, checkBase)
+
+	var checkWrapper *wrapperErr
+	assert.True(t, oops.As(c, &checkWrapper))
+	assert.Equal(t, middle, checkWrapper)
+
+	checkWrapper = nil
+	assert.True(t, oops.As(d, &checkWrapper))
+	assert.Equal(t, middle, checkWrapper)
 }
